be9ebcf8b141f6653548df44fda025faac240f8c
Nik Atlas <nikatlas@gmail.com>
2017-11-01 18:34:17 +0200

    Initial commit

---
+++ src/components/Catalogue.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Catalogue extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
