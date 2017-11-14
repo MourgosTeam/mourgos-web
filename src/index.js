c78d31844620bb5247c285d8b22ddd684d0706b0
Nik Atlas <nikatlas@gmail.com>
2017-11-14 22:36:43 +0200

    Fix semicolon warning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 import 'bootstrap/dist/css/bootstrap-reboot.css';
-import 'font-awesome/css/font-awesome.css'
+import 'font-awesome/css/font-awesome.css';
 import './index.jsx';
