b2085279eba1b1e961b526431f2e8b842274d3a7
Nik Atlas <nikatlas@gmail.com>
2018-01-10 15:23:35 +0200

    Lint Fix

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,19 +1,29 @@
-let statusText = ['ΝΕΑ ΠΑΡΑΓΓΕΛΙΑ','ΕΤΟΙΜΑΖΕΤΑΙ', 'ΕΤΟΙΜΑΣΤΗΚΕ', 'ΣΤΟΝ ΔΡΟΜΟ'];
+const alertDelay = 10,
+    extraCharge = 0.50,
+    gainMultiplier = 0.15,
+    lineColor = [
+        '',
+        '',
+        'table-primary',
+        'table-info'
+    ],
+    statusText = [
+        'ΝΕΑ ΠΑΡΑΓΓΕΛΙΑ',
+        'ΕΤΟΙΜΑΖΕΤΑΙ',
+        'ΕΤΟΙΜΑΣΤΗΚΕ',
+        'ΣΤΟΝ ΔΡΟΜΟ'
+    ];
+
 statusText[10] = 'ΟΛΟΚΛΗΡΩΘΗΚΕ';
 statusText[99] = 'ΑΠΟΡΡΙΦΘΗΚΕ';
 
-let lineColor = ['','', 'table-primary', 'table-info'];
 lineColor[10] = 'table-success';
 lineColor[99] = 'table-danger';
 
-const gainMultiplier = 0.15;
-const extraCharge = 0.50;
-const alertDelay = 10;
-
 export default {
-	alertDelay,
-	lineColor,
-	statusText,
-	gainMultiplier,
-	extraCharge
-}
+    alertDelay,
+    extraCharge,
+    gainMultiplier,
+    lineColor,
+    statusText
+};
